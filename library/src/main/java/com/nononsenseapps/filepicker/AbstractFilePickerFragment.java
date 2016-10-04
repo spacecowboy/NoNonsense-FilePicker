bc7755aee1a22a0041262f616c6f0f1811834e38
contrudar <urzhumcevav@gmail.com>
2016-10-04 16:17:04 +0300

    make DialogFragment from Fragment File Picker

--- library/src/main/java/com/nononsenseapps/filepicker/AbstractFilePickerFragment.java
+++ library/src/main/java/com/nononsenseapps/filepicker/AbstractFilePickerFragment.java
@@ -15,7 +15,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -50,7 +50,7 @@ import static com.nononsenseapps.filepicker.Utils.appendPath;
  * OnFilePickedListener}
  * interface.
  */
-public abstract class AbstractFilePickerFragment<T> extends Fragment
+public abstract class AbstractFilePickerFragment<T> extends DialogFragment
         implements LoaderManager.LoaderCallbacks<SortedList<T>>,
         NewItemFragment.OnNewFolderListener, LogicHandler<T> {
 
