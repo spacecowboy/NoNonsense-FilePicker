e21ad09454f1038edf548669ec016af090162854
Arhipov <gorodeckii@gmail.com>
2017-04-06 16:40:38 +0300

    deleted tests, examples and unnecessary dependencies

--- library/src/main/java/com/nononsenseapps/filepicker/AbstractFilePickerFragment.java
+++ library/src/main/java/com/nononsenseapps/filepicker/AbstractFilePickerFragment.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -49,7 +50,7 @@ import static com.nononsenseapps.filepicker.Utils.appendPath;
  * OnFilePickedListener}
  * interface.
  */
-public abstract class AbstractFilePickerFragment<T> extends Fragment
+public abstract class AbstractFilePickerFragment<T> extends DialogFragment
         implements LoaderManager.LoaderCallbacks<SortedList<T>>,
         NewItemFragment.OnNewFolderListener, LogicHandler<T> {
 
--- sample/src/main/java/com/nononsenseapps/filepicker/sample/fastscroller/FastScrollerFilePickerActivity2.java
+++
@@ -1,7 +0,0 @@
-package com.nononsenseapps.filepicker.sample.fastscroller;
-
-/**
- * Just for theme sample purposes
- */
-public class FastScrollerFilePickerActivity2 extends FastScrollerFilePickerActivity {
-}
